b25e2efb598d4c2895c323c0821375028a906319
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-02-18 11:35:17 +0100

    background

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -8,6 +8,11 @@ export default function App() {
       
 
       <View style={styles.carContainer}>
+
+        <ImageBackground 
+        source={require('./assets/images/ModelX.jpeg')}
+        style={styles.image}
+        />
 
         <View style={styles.titles}>
           <Text style={styles.title}>Model S</Text>
@@ -41,10 +46,18 @@ const styles = StyleSheet.create({
 
   title: {
     fontSize: 40,
-    fontWeight: 'bold',
+    fontWeight: '500',
   },
 
   subtitle: {
+    fontSize: 16,
+    color: '#5c5e62'
+  },
 
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'cover',
+    position: 'absolute'
   }
 });
