24c974af1ac1c293401865b5d71d7bd24e35f7d3
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-03-10 11:04:01 +0100

    carslist

---
+++ components/CarsList/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import styles from './styles';
+import cars from './cars';
+
+const CarsList = (props) => {
+    return (
+        <View>
+            <Text>component</Text>
+        </View>
+    );
+};
+
+export default CarsList;
