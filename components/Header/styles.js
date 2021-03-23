47b99a600a6944b4d46e6b608f73df3a774ff35f
Ali Yildiz <yildiz.ali.s1990@gmail.com>
2021-03-23 13:59:10 +0100

    Header styles

--- components/Header/styles.js
+++ components/Header/styles.js
@@ -2,13 +2,16 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-
+        position: 'absolute',
+        top: 50,
     },
     logo: {
-
+        width: 100,
+        height: 20,
     },
     menu: {
-
+        width: 25,
+        height: 25,
     }
 });
 
